9f6373df366f0e13be1b82ac9301815be616080f
Olivier <olivergg@users.noreply.github.com>
2015-01-05 22:49:44 +0100

    Problem 19 solved

--- src/experiment/core.clj
+++ src/experiment/core.clj
@@ -1052,9 +1052,8 @@ i = index of rows, j index of column
 ;;; problem 19
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; 7 janvier 1900 was a sunday
+;; january 7th 1900 was a sunday
+;; january 6th 1901 was a sunday
 (defn iscentury
   [year]
   (= 0 (mod year 100))
@@ -1073,24 +1072,45 @@ i = index of rows, j index of column
 (assert (= true (isleapyear 2000)))
 (assert (= false (isleapyear 1900)))
 
-(defn incl-range
-  [s,f]
-  (range s (inc f))
+(defn daysinmonth
+  [n,year]
+  (cond
+    (or (= n 9) (= n 4) (= n 6) (= n 11)) 30
+    (= n 2) (if (isleapyear year) 29 28) 
+    :else 31
+    )
   )
+(assert (= 31 (daysinmonth 1 1900)))
+(assert (= 30 (daysinmonth 11 1900)))
+(assert (= 28 (daysinmonth 2 1900)))
+(assert (= 29 (daysinmonth 2 1996)))
 
-(incl-range 1 31)
-(incl-range 1 28)
-
-
-(defprotocol IDate
-   (printdate [this])
-   (add [this,nbdays])
-   )
-
-(defrecord Date [day month year]
-  IDate
-  (printdate [this] (println day "/" month "/" year))
-  (add [this,other] (printdate other))
+(defn add 
+  "Add the given number of days to the given date. Returns a (day,month,year) list"
+  [day month year nbdays]
+  (def daysinm (daysinmonth month year))
+  (def diff (- daysinm (+ day nbdays)))
+  (def remain (- nbdays (- daysinm day) 1));; remove 1 to the remain, since the first day of the next month is counted.
+  (if (>= diff 0)
+    (list (+ day nbdays) month year)
+    (do
+      (def incmonth (inc month))
+      (def alteredmonth (if (= incmonth 13) 1 incmonth))
+      (def alteredyear (if (= incmonth 13) (inc year) year))
+      (recur 1 alteredmonth alteredyear remain)
+      )
+    )
   )
+(time (loop [iter 1, count 0]
+        (def res (add 6 1 1901 (* 7 iter)))
+          (if (= 2001 (last res))
+            count 
+            (if (= 1 (first res));; first day of the month
+              (recur (inc iter) (inc count))
+              (recur (inc iter) count)
+              )
+            )
+        ))
+;171
 
-(add (->Date 7 1 1900) (->Date 7 1 1901))
+
