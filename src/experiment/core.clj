df0369954995db37f13d01433ca93c1d66a73091
Olivier <olivergg@users.noreply.github.com>
2015-01-10 18:05:26 +0100

    Work in progress (prob 20)

--- src/experiment/core.clj
+++ src/experiment/core.clj
@@ -8,12 +8,14 @@
 (def x 3)
 
 
-(def factorial
-  (fn [n]
-    (loop [cnt n, acc 1]
-      (if (= 0 cnt)
-        acc
-        (recur (dec cnt) (* acc cnt))))))
+(defn factorial
+  [n]
+  (loop [cnt n, acc 1]
+    (if (= 0 cnt)
+      acc
+      (recur (dec cnt) (* acc cnt)))
+    )
+  )
 
 
 (factorial 6)
@@ -1113,4 +1115,98 @@ i = index of rows, j index of column
         ))
 ;171
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; problem 20
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
+(defn sdiv
+  [n,d]
+  (list (quot n d) (mod n d))
+  )
+
+
+(defn decimalsplit
+  [n]
+  (sdiv n 10)
+  )
+
+(defn generate0 [c] (map (fn [x] 0) (range 0 c)))
+
+(defn conjj
+  [coll,x]
+  (if (zero? x)
+    coll
+    (conj coll x)
+    )
+  )
+  
+  
+(defn aline
+  [initx, initcoll]
+  
+  (loop [x initx,remain 0, coll initcoll,out ()]
+    (if (empty? coll)
+      (reverse (conjj out remain))
+      (do
+        (def y (first coll))
+        (def xy (+ (* x y) remain))
+        (let [[xydec xyunit] (decimalsplit xy)]
+          (recur x xydec (rest coll) (conj out xyunit))
+          )
+        )
+      )
+    )
+  )
+
+(defn thelines
+  [initxcoll, initcoll]
+  (loop [iter 0, xcoll initxcoll, coll initcoll, out ()]
+    (if (empty? xcoll)
+      out
+      (do
+        (def fi (first xcoll))
+        (def zeros (generate0 iter))
+        (def newout (if (= 0 iter)
+                      (conj out (aline fi coll))
+                      (conj out (concat zeros (aline fi coll)))
+;                      (conj out (aline fi coll))
+                      ))
+        (recur (inc iter) (rest xcoll) coll newout)
+        
+        )
+      )
+    )
+  )
+
+
+(thelines '(0 1) '(2 3))
+
+(first ())
+
+(defn sumcoll
+  [initcoll1, initcoll2]
+  (loop [coll1 initcoll1, coll2 initcoll2, remain 0, out ()]
+    (cond
+      (and (empty? coll1) (empty? coll2)) (conjj out remain)
+      :else (do
+              (def fcol1 ((fnil identity 0) (first coll1)))
+              (def fcol2 ((fnil identity 0) (first coll2)))
+              (def decsplit (decimalsplit (+ fcol1 fcol2)))
+              (recur (rest coll1) (rest coll2) (first decsplit) (conj out (last decsplit)))
+              )
+      )
+    )
+  )
+  
+
+(rest (rest (rest '(0 1 2))))
+(sumcoll '(9 1 2) '(1 8))
+
+(+ 219 81)
 
