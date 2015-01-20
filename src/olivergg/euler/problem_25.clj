7d82783c50213401c678826ddea061d7308e7f56
Olivier <olivergg@users.noreply.github.com>
2015-01-20 21:05:52 +0100

    Problem 25 solved

---
+++ src/olivergg/euler/problem_25.clj
@@ -0,0 +1,30 @@
+(ns olivergg.euler.problem_25
+  (:require [olivergg.euler.common :refer :all]
+            [clojure.set :refer :all]
+            [clojure.math :refer :all]
+            ))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; problem 25
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; for large n, the nth fibo number is the closest integer to gold^n/sqrt(5)
+;; the number of digits of a number x can be obtained from log10(x)
+;;therefore, the number of digits of the nth fibo number is :
+
+;; log10(  gold^n / sqrt(5) ) = n log10 (gold) - log10(sqrt(5))
+;; we are looking for n such that n log10 (gold) - log10(sqrt(5)) +1 = 1000
+
+(def gold (* 0.5 (+ 1 (Math/sqrt 5))))
+
+(loop [n 1]
+  (def cpt (+ (- (* n (Math/log10 gold)) (Math/log10 (Math/sqrt 5)) ) 1))
+  (if (>= cpt 1000)
+    n
+    (recur (inc n))
+    )
+  )
+
+;4782
