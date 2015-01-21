01be7630a4a4ea1e7286b2e142c7ab7731f70ca2
Olivier <olivergg@users.noreply.github.com>
2015-01-21 19:40:18 +0100

    Problem 26 solved

--- src/olivergg/euler/problem_26.clj
+++ src/olivergg/euler/problem_26.clj
@@ -8,3 +8,29 @@
 ;;; problem 26
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn periodlength
+  "Returns the period length of the decimal representation of inita/initb"
+  [inita,initb]
+  (loop [iter 0, a inita, b initb, restset #{}]
+    (def Q (quot a b))
+    (def r (mod a b))
+    (if (contains? restset r)
+      (do
+        ;(println "last q " Q)
+        ;(println "iter " iter)
+        iter
+        )
+      (do
+        ;(println "q" Q)
+        (recur (inc iter) (* 10 r) b, (conj restset r))
+        )
+      )
+    )
+  )
+
+(last (sort-by (fn[x] (x :len)) (for [d (range 1 1001)
+      :let [plen (periodlength 1 d)]]
+  {:d d :len plen }
+  )))
+; 983
+
