d354e141c0e31179979d4842d1ea15efac70ecb5
Olivier <olivergg@users.noreply.github.com>
2015-03-05 20:02:14 +0100

    Problem 46 solved

---
+++ src/olivergg/euler/problem_46.clj
@@ -0,0 +1,31 @@
+(ns olivergg.euler.problem_46
+  (:require [olivergg.euler.common :refer :all]
+            [clojure.set :refer :all]
+            )
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; problem 46
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn isdecomp
+  [oddnum]
+  (def primesbelow (generate-prime-numbers oddnum 1))
+  (def twicesquares (map #(* 2 (* % %)) (range 1 (inc (int (Math/sqrt (/ oddnum 2)))))))
+  (first (for [twsq twicesquares
+               pri primesbelow
+               :when (= oddnum (+ twsq pri))
+               ]
+           true
+           )
+         )
+  )
+
+(first (for [i (filter #(not (isprime %))  (range 7 30000 2))
+             :when (nil? (isdecomp i))]
+         i
+         )
+       )
+
+;5777
