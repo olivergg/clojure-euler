aec3d1e9a8a1727554eb0399184db3b3478f96e8
Olivier <olivergg@users.noreply.github.com>
2015-03-08 20:36:06 +0100

    Problem 47 solved (but slow)

---
+++ src/olivergg/euler/problem_47.clj
@@ -0,0 +1,46 @@
+(ns olivergg.euler.problem_47
+  (:require [olivergg.euler.common :refer :all]
+            [clojure.set :refer :all]
+            )
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; problem 47
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn hasKDistinctFactor
+  [initn, K]
+  (if (isprime initn)
+    false
+    (loop [n initn
+           k 2
+           acc #{}
+           iter 0]
+      (if (or (= 1 n) (> (count acc) K))
+        (= K (count acc))
+        (if (= 0 (mod n k))
+          (recur (quot n k) k (conj acc k) (inc iter))
+          (recur n (inc k) acc iter)
+          )
+        )
+      )
+    )
+  )
+
+(sumproperdivisor (! 12))
+
+(def hasmemo hasKDistinctFactor)
+
+(time (first (for [i (range 1 200000)
+             :when (and
+                     (hasmemo i 4)
+                     (hasmemo (+ i 1) 4)
+                     (hasmemo (+ i 2) 4)
+                     (hasmemo (+ i 3) 4)
+                      )
+             ]
+         i
+         )
+       ))
+;134043
