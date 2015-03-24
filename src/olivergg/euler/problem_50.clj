4e413e14d65c06c6d23a608693b229bf2a76a692
Olivier <olivergg@users.noreply.github.com>
2015-03-24 17:42:38 +0100

    Minor fix

--- src/olivergg/euler/problem_50.clj
+++ src/olivergg/euler/problem_50.clj
@@ -36,55 +36,13 @@
   )
 
 
-
-;(filter #(and (isprime (:sum %)) (< (:sum %) 1000)) (cumulsumprimes 2 1000))
-;;{:sum 281, :length 14}
-;(filter #(and  (< (:sum %) 1000) (> (:sum %) 281) (>= (:length %) 15) (isprime (:sum %)))   (cumulsumprimes 3 1000))
-;; {:sum 499, :length 17}
-;(filter #(and  (< (:sum %) 1000) (> (:sum %) 499) (>= (:length %) 18) (isprime (:sum %)))   (cumulsumprimes 5 1000))
-;; {:sum 563, :length 17}
-;(filter #(and  (< (:sum %) 1000) (> (:sum %) 563) (>= (:length %) 18) (isprime (:sum %)))   (cumulsumprimes 7 1000))
-;;{:sum 953, :length 21}
-;
-;(empty? (intersection
-;          (into #{} (generate-prime-numbers 1000 953))
-;          (into #{} (cumulsumprimes 2 100))
-;          )
-;        )
-;;STOP
-(cumulsumprimes 2 1000)
-(generate-prime-numbers 1000 953)
-
-;
-;
-;(filter #(and (isprime (:sum %)) (< (:sum %) 1000000)) (cumulsumprimes 2 1000000))
-;;{:sum 281, :length 14}
-;(filter #(and  (< (:sum %) 1000000) (>= (:sum %) 958577) (>= (:length %) 537) (isprime (:sum %)))   (cumulsumprimes 3 1000000))
-;; continue
-;(filter #(and  (< (:sum %) 1000000) (>= (:sum %) 958577) (>= (:length %) 537) (isprime (:sum %)))   (cumulsumprimes 5 1000000))
-;; {:sum 978037, :length 539}
-;(filter #(and  (< (:sum %) 1000000) (>= (:sum %) 978037) (>= (:length %) 540) (isprime (:sum %)))   (cumulsumprimes 7 1000000))
-;;{:sum 997651, :length 543}
-;
-;(empty? (intersection
-;          (into #{} (generate-prime-numbers 1000000 997651))
-;          (into #{} (cumulsumprimes 2 1000000))
-;          )
-;        )
-;
-;; 997651
-
-
-
 (defn findlongestsum
   [n]
   (loop [currentmaxsum 0
          currentmaxlength 0
-         primestoiter (generate-prime-numbers 23 1)
-         ;stopcond false
+         primestoiter (generate-prime-numbers 7 1)
          ]
-        (if (or (empty? primestoiter)
-                ;stopcond
+        (if (or (empty? primestoiter) ;TODO : find another stop condition
                 )
           {:sum currentmaxsum :length currentmaxlength}
           (do
@@ -97,15 +55,9 @@
                                         (isprime (:sum %)))
                                       cumulprimefromp)
               )
-            (println (count listtosearch))
             (def maxk (apply max-key :sum (if (empty? listtosearch) (list 0) listtosearch )))
             (def newmaxsum (:sum maxk))
             (def newmaxlength (:length maxk))
-            ;(def newstopcond (empty? (intersection
-            ;          (into #{} (generate-prime-numbers n currentmaxsum))
-            ;          (into #{} cumulprimefromp)
-            ;          )
-            ;        ))
             (if (and (not (nil? newmaxsum))
                      (not (nil? newmaxlength))
                      (> newmaxlength currentmaxlength)
@@ -118,7 +70,5 @@
           )
         )
   )
-(count (filter #(and (isprime (:sum %)) (< (:sum %) 1000000))  (cumulsumprimes 2 1000000)))
-
 (time (findlongestsum 1000000))
 ; {:sum 997651, :length 543}
