b0e1d5ec5d02b62281dcef7c3f79093e1d9d8d3d
Olivier <olivergg@users.noreply.github.com>
2015-05-25 20:47:56 +0200

    Improve documentation in problem 31

--- src/olivergg/euler/problem_31.clj
+++ src/olivergg/euler/problem_31.clj
@@ -18,34 +18,55 @@
 ; let's remind that (1/(1-x)) = 1 + x + x^2 + x^3 + x^4 + ....
 ; let's define a polynomial P = a0 + a1 * X + a2 * X^2 + a3 * X^3 + ... by its coefficient (a0, a1, a2, a3, ....)
 
-(defn mulpol
-  "polynomial multiplication"
+
+(defrecord Monomial [power value])
+
+(defn mulpolExpand
+  "Polynomial multiplication.
+  The result is not a polynomial but a sequence of Monomial "
   [pol1, pol2]
-  (def expanded-terms (for [i (range 0 (count pol1))
-                 j (range 0 (count pol2))
-                 ]
-               {:sumofpower (+ i j)
-                :termsproduct (* (pol1 i) (pol2 j))
-                }
-             )
+  (for [i (range 0 (count pol1))
+        j (range 0 (count pol2))
+        :let [pol1i (pol1 i)
+              pol2j (pol2 j)
+              product (* pol1i pol2j)
+              ]
+        ]
+    (->Monomial (+ i j) product)
     )
-  (into [] (map (fn [[pow mappowvals]]
-                  (reduce + (map (fn [x] (x :termsproduct)) mappowvals))
-                  )
-                (into (sorted-map) (group-by (fn [mappowval] (mappowval :sumofpower))
-                                             expanded-terms
-                                             )
-                      )
-                )
+  )
+
+(defn simplify
+  "Simplify into a polynomial a sequence of monomials "
+  [monomialSeq]
+  (into [] (map
+             ;group by power, sort by power the resulting map, and sum the values with the same power.
+             (fn [[k monomialsOfPowerK]]
+               (reduce + (map #(:value %) monomialsOfPowerK))
+               )
+             (into (sorted-map) (group-by #(:power %) monomialSeq))
+             )
         )
   )
+
+(defn mulpol
+  "Polynomial multiplication. The result is also a polynomial"
+ [pol1, pol2]
+ (simplify (mulpolExpand pol1 pol2))
+ )
+
+
 (defn genvecforn
-  "generates a list of size n that contains only 0 except the first elem
+ "Generates a list of size n that starts with 1 followed by a sequence of 0.
   e.g. : n = 4 => '(1 0 0 0)
   "
   [n]
-  (conj (repeat (dec n) 0) 1)
+  (conj (repeat (- n 1) 0) 1)
   )
+
+
+
+
 (defn expand-geomseries
   "Expand the series 1/1-x^n up to the given maximum degree maxpow
   e.g : 1/1-x^2 => n=2, maxpow=10 => [1 0 1 0 1 0 1 0 1 0 1]
