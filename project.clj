990b9ba469191ee5f375e784dc8b4ecc6f0b41d4
Olivier <olivergg@users.noreply.github.com>
2015-09-14 20:49:45 +0200

    Add lein2eclipse plugin and timbre library

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/olivergg/clojure-euler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.taoensso/timbre "3.4.0"]
                  ]
   :resource-paths ["shared" "resources"]
+  :plugins [[lein2-eclipse "2.0.0"]]
   )
